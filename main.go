bd6b01aa4db17e6d45422e110400629887249316
Chris Vaughn <chris@cvaughn.com>
2020-04-16 13:39:06 -0500

    Add po case

--- main.go
+++ main.go
@@ -13,7 +13,12 @@ func main() {
 	arLocale := gotext.NewLocale("locale", "ar_SA")
 	arLocale.AddDomain("messages")
 
+	po := new(gotext.Po)
+	po.ParseFile("locale/ar_SA/LC_MESSAGES/messages.po")
+
 	fmt.Printf("English With Just a Get: %s\n", enLocale.Get("This is a sample string"))
 	fmt.Printf("Arabic With Just a Get: %s\n", arLocale.Get("This is a sample string"))
 	fmt.Printf("Arabic Plural To 0: %s\n", arLocale.GetN("This is a sample string", "This is a sample string", 0))
+	fmt.Printf("Arabic Using po.Get: %s\n", po.Get("This is a sample string"))
+
 }
